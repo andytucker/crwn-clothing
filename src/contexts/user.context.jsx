71d88e8ca26038db71b945f544154d51b94fb565
Victor Pilyasinsky <vip23@bk.ru>
2023-01-16 12:06:43 +0300

    switched to reducers for Cart & User

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,6 +1,8 @@
 import { createContext, useEffect, useReducer } from "react";
 import { onAuthStateChangedListener,         createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
 
+import { createAction } from "../utils/reducer/reducer.utils";
+
 
 export const UserContext = createContext({
     currentUser: null,
@@ -34,20 +36,22 @@ export const UserProvider = ({ children }) => {
 
     const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
    
-    const setCurrentUser = (user) => {
-        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
-    };
-
-    const value = { currentUser, setCurrentUser };
+    const setCurrentUser = (user) => 
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   
-    useEffect(() => {
-       const unsubscribe = onAuthStateChangedListener((user) => {
+        const value = { currentUser, setCurrentUser };
+
+        useEffect(() => {
+        const unsubscribe = onAuthStateChangedListener((user) => {
         if(user) {
             createUserDocumentFromAuth(user);
         }
         setCurrentUser(user);
-       });
-       return unsubscribe;
-    }, []);
+        });
+        return unsubscribe;
+        }, []);
+    
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+
+
 };
